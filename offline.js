46d18faaae0406dff60cfc4352cd7ab7c4df9ebe
emrahtobbas <98615494+emrahtobbas@users.noreply.github.com>
2022-02-07 21:58:24 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1644260174,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/tiledbackground3.png",
+		"images/zemin-sheet0.png",
+		"images/ilerispt-sheet0.png",
+		"images/bilgispt-sheet0.png",
+		"images/arkaplan-sheet0.png",
+		"images/cocuk1-sheet0.png",
+		"images/cocuk1-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"sorular..txt"
+	]
+}
